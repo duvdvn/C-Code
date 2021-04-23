69c854b2e094b2536d8b3f9403050ce04de5be51
Vu Thanh Du <duvdvn@gmail.com>
2021-04-23 08:19:01 +0700

    Update Baitap05.c

--- Task7/Baitap05.c
+++ Task7/Baitap05.c
@@ -3,7 +3,7 @@
 void main ()
 {
 	int	bangdiem[4][30];
-	int tongdiem[30];
+	float tong;
 	int diem, sv, sl;
 	
 	printf ("Nhap vao so luong Sinh vien: ");
@@ -14,32 +14,19 @@ void main ()
 		for (sv=0; sv<sl; sv++)
 		{
 			printf ("Nhap diem Mon %d cua SV %d: ", diem+1, sv+1);
-			scanf ("%d", &bangdiem[diem][sv]);
-			
-		}
-	}
-	
-	for (diem=0; diem<4; diem++)
-	{
-		for (sv=0; sv<sl; sv++)
-		{
-			tongdiem[sv]+=tongdiem[sv]+bangdiem[diem][sv];
-		}
-	}	
-	
-	printf("\nBang ket qua hoc tap: \n");
-	for (diem=0; diem<4; diem++)
-	{
-		printf ("\n");
-		for (sv=0; sv<sl; sv++)
-		{
-			printf ("\t%d", bangdiem[diem][sv]);
+			scanf ("%d", &bangdiem[diem][sv]);	
 		}
 	}
 	
 	for (sv=0; sv<sl; sv++)
-		printf ("\n\t%d", tongdiem[sv]);
-	
+	{
+		tong=0;
+		for (diem=0; diem<4; diem++)
+		{
+			tong+=(float)bangdiem[diem][sv];
+		}
+		printf ("\nDiem trung binh cua SV%d = %.2f", sv+1, tong/4);
+	}		
 }
 
 
